058c290a0becbe8fed10bcea76f969e55347be80
sangyeop <yeop1.dev@gmail.com>
2023-06-17 01:41:35 +0900

    :recycle: refactor(Alarams): 알람 맞추는 기능 변경

--- src/main/java/io/gitgub/yeop/alarm_clock/Alarms.java
+++ src/main/java/io/gitgub/yeop/alarm_clock/Alarms.java
@@ -1,9 +1,8 @@
 package io.gitgub.yeop.alarm_clock;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-final class Alarms {
+final class Alarms extends Thread {
     private final List<Time> times;
 
     public Alarms() {
@@ -12,5 +11,23 @@ final class Alarms {
 
     public void add(Time time) {
         times.add(time);
+    }
+
+    /**
+     * ifPresentOrElse  - 값이 존재할때와 존재하지 않을때 사용
+     * ifPresent - 값이 존재할때만 사용
+     */
+    @Override
+    public void run() {
+        while (true) {
+            catcher(Clock.start(Time::new)).ifPresent(action -> System.out.println("알람이 울립니다!!!"));
+        }
+    }
+
+    public Optional<Time> catcher(Time time) {
+        if (times.removeIf(time::equals)) {
+            return Optional.of(time);
+        }
+        return Optional.empty();
     }
 }
