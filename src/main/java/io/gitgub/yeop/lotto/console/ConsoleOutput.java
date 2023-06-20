669ca772da270db8f0866d211b93be5e434e43c2
sangyeop <yeop1.dev@gmail.com>
2023-06-21 00:07:40 +0900

    :sparkles: feat(ConsoleOutput): 전체적인 출력물을 관리한다.

---
+++ src/main/java/io/gitgub/yeop/lotto/console/ConsoleOutput.java
@@ -0,0 +1,33 @@
+package io.gitgub.yeop.lotto.console;
+
+import io.gitgub.yeop.lotto.Buy;
+
+public class ConsoleOutput {
+    private static final String BUY_AMOUNT_MESSAGE = "구입금액을 입력해 주세요. (1_000원 = 1장)";
+    private static final String BUY_CONFIRMATION_MESSAGE = "로또 %s장을 구매했습니다.";
+
+    public void printBuyAmountMessage() {
+        println(BUY_AMOUNT_MESSAGE);
+    }
+
+    public void printBuyConfirmationMessage(int buyOfNumber) {
+        printf(BUY_CONFIRMATION_MESSAGE, buyOfNumber);
+        newLine();
+    }
+
+    public void printLottoNumbers(Buy results) {
+        println(results.stringValue());
+    }
+
+    private void println(String msg) {
+        System.out.println(msg);
+    }
+
+    private void printf(String msg, Object... objects) {
+        System.out.printf(msg, objects);
+    }
+
+    private void newLine() {
+        System.out.println("");
+    }
+}
