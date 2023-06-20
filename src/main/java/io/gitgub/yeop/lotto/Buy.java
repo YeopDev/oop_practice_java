824416ef13070463262523fc51829cf7cbaf05c6
sangyeop <yeop1.dev@gmail.com>
2023-06-21 00:06:24 +0900

    :sparkles: feat(Buy): 구입한 로또용지를 갖는다.

---
+++ src/main/java/io/gitgub/yeop/lotto/Buy.java
@@ -0,0 +1,25 @@
+package io.gitgub.yeop.lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.lineSeparator;
+import static java.util.stream.Collectors.joining;
+
+public class Buy {
+    private List<Lotto> results;
+
+    public Buy() {
+        this.results = new ArrayList<>();
+    }
+
+    public void add(Lotto target) {
+        results.add(target);
+    }
+
+    public String stringValue() {
+        return results.stream()
+                .map(Lotto::numbers)
+                .collect(joining(lineSeparator()));
+    }
+}
