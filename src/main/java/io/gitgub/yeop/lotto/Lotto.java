979ea846f8e8634fdb480c1ed7d832832e0e6e1d
sangyeop <yeop1.dev@gmail.com>
2023-06-22 00:21:04 +0900

    :recycle: refactor(Lotto): parameter String -> List<Integer> 변경

--- src/main/java/io/gitgub/yeop/lotto/Lotto.java
+++ src/main/java/io/gitgub/yeop/lotto/Lotto.java
@@ -1,5 +1,20 @@
 package io.gitgub.yeop.lotto;
 
-public record Lotto(String numbers) {
+import java.util.List;
 
+public record Lotto(List<Integer> numbers) {
+    public Lotto{
+        validate(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return "번호: " + numbers;
+    }
+
+    private void validate(List<Integer> numbers){
+        if(numbers == null || numbers.isEmpty()){
+            throw new IllegalArgumentException("번호가 생성된 시점에는 비어있을 수 없습니다.");
+        }
+    }
 }
