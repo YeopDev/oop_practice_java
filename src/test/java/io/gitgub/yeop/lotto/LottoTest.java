309774d3c1831e8568654f8e8f9a664cf94908b3
sangyeop <yeop1.dev@gmail.com>
2023-06-22 02:37:47 +0900

    :white_check_mark: Test(LottoTest): 로또번호 생성 테스트

--- src/test/java/io/gitgub/yeop/lotto/LottoTest.java
+++ src/test/java/io/gitgub/yeop/lotto/LottoTest.java
@@ -2,15 +2,16 @@ package io.gitgub.yeop.lotto;
 
 import io.gitgub.yeop.lotto.create.CreateLottoNumbers;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class LottoTest {
-
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {6})
     @DisplayName("로또 번호 생성")
-    void createLottoNumbers() {
-        assertDoesNotThrow(() -> new Lotto(CreateLottoNumbers.AUTO.create(6)));
+    void createLottoNumbers(int size) {
+        assertDoesNotThrow(() -> new Lotto(CreateLottoNumbers.AUTO.create(size)));
     }
 }
