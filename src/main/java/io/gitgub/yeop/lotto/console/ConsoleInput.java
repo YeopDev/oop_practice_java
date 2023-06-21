eee0f49545b34a8b50b2feef7d801f76f5de93fd
sangyeop <yeop1.dev@gmail.com>
2023-06-22 02:34:46 +0900

    :recycle: refactor(ConsoleInput): 유효성검사 추가

--- src/main/java/io/gitgub/yeop/lotto/console/ConsoleInput.java
+++ src/main/java/io/gitgub/yeop/lotto/console/ConsoleInput.java
@@ -3,6 +3,7 @@ package io.gitgub.yeop.lotto.console;
 import java.util.Scanner;
 
 public class ConsoleInput {
+    private static final int MINIMUM_INPUT = 1_000;
     private final Scanner scanner;
 
     public ConsoleInput() {
@@ -10,6 +11,10 @@ public class ConsoleInput {
     }
 
     public int buyAmount() {
-        return scanner.nextInt();
+        int input = scanner.nextInt();
+        if (input < MINIMUM_INPUT) {
+            throw new IllegalArgumentException("로또 구입을 위한 지불금액은 최소 %d원 이상 입력해야합니다. (현재 값 %d)".formatted(MINIMUM_INPUT, input));
+        }
+        return input;
     }
 }
