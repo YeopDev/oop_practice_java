2c6a9e508f8ae1c3d99fb4dd4956fafdf783e015
sangyeop <yeop1.dev@gmail.com>
2023-06-22 00:25:41 +0900

    :recycle: refactor(ConsoleOutput): 콘솔 입력 메시지 변경

--- src/main/java/io/gitgub/yeop/lotto/console/ConsoleOutput.java
+++ src/main/java/io/gitgub/yeop/lotto/console/ConsoleOutput.java
@@ -4,7 +4,7 @@ import io.gitgub.yeop.lotto.Buy;
 
 public class ConsoleOutput {
     private static final String BUY_AMOUNT_MESSAGE = "구입금액을 입력해 주세요. (1_000원 = 1장)";
-    private static final String BUY_CONFIRMATION_MESSAGE = "로또 %s장을 구매했습니다.";
+    private static final String BUY_CONFIRMATION_MESSAGE = "총 %s장을 구매했습니다.";
 
     public void printBuyAmountMessage() {
         println(BUY_AMOUNT_MESSAGE);
