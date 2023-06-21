c80c1c550f7263db95913202697c6dd73097a7dc
sangyeop <yeop1.dev@gmail.com>
2023-06-22 02:35:35 +0900

    :recycle: refactor(Lotto): 유효성검사 삭제

--- src/main/java/io/gitgub/yeop/lotto/Lotto.java
+++ src/main/java/io/gitgub/yeop/lotto/Lotto.java
@@ -3,18 +3,8 @@ package io.gitgub.yeop.lotto;
 import java.util.List;
 
 public record Lotto(List<Integer> numbers) {
-    public Lotto{
-        validate(numbers);
-    }
-
     @Override
     public String toString() {
         return "번호: " + numbers;
-    }
-
-    private void validate(List<Integer> numbers){
-        if(numbers == null || numbers.isEmpty()){
-            throw new IllegalArgumentException("번호가 생성된 시점에는 비어있을 수 없습니다.");
-        }
     }
 }
