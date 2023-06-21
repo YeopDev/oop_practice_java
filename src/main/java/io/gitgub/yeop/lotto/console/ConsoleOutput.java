61866fbf5aac81d4189295498520a06674d31912
sangyeop <yeop1.dev@gmail.com>
2023-06-22 02:35:06 +0900

    :recycle: refactor(ConsoleOutput): 유효성검사 추가

--- src/main/java/io/gitgub/yeop/lotto/console/ConsoleOutput.java
+++ src/main/java/io/gitgub/yeop/lotto/console/ConsoleOutput.java
@@ -3,6 +3,7 @@ package io.gitgub.yeop.lotto.console;
 import io.gitgub.yeop.lotto.Buy;
 
 public class ConsoleOutput {
+    private static final int MINIMUM_BUY_SHEET = 1;
     private static final String BUY_AMOUNT_MESSAGE = "구입금액을 입력해 주세요. (1_000원 = 1장)";
     private static final String BUY_CONFIRMATION_MESSAGE = "총 %s장을 구매했습니다.";
 
@@ -11,6 +12,9 @@ public class ConsoleOutput {
     }
 
     public void printBuyConfirmationMessage(int buyOfNumber) {
+        if (buyOfNumber < MINIMUM_BUY_SHEET) {
+            throw new IllegalArgumentException("최소 %d장 이상 구입해야 합니다. (현재 값 %d)".formatted(MINIMUM_BUY_SHEET, buyOfNumber));
+        }
         printf(BUY_CONFIRMATION_MESSAGE, buyOfNumber);
         newLine();
     }
