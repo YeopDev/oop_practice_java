eb16ad71ab45063b451a64b481dd4cb6f5d8364e
sangyeop <yeop1.dev@gmail.com>
2023-06-22 00:25:02 +0900

    :recycle: refactor(CreateLottoNumbers): String -> List<Integer> 변경

--- src/main/java/io/gitgub/yeop/lotto/create/CreateLottoNumbers.java
+++ src/main/java/io/gitgub/yeop/lotto/create/CreateLottoNumbers.java
@@ -1,20 +1,17 @@
 package io.gitgub.yeop.lotto.create;
 
+import java.util.List;
 import java.util.random.RandomGenerator;
-
-import static java.util.stream.Collectors.joining;
 
 @FunctionalInterface
 public interface CreateLottoNumbers {
-    String create(int SIZE);
+    List<Integer> create(int SIZE);
 
     CreateLottoNumbers AUTO = (int SIZE) -> RandomGenerator.getDefault()
             .ints(1, 45)
             .distinct()
             .limit(SIZE)
             .sorted()
-            .mapToObj(String::valueOf)
-            .collect(
-                    joining(", ", "[", "]")
-            );
+            .boxed()
+            .toList();
 }
